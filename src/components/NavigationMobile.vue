18ac818cd37539d4fe0eb6415c50e164a763b004
Yang Edruce <nurulizyansyaza@gmail.com>
2024-05-07 09:49:41 +0800

    update: availability color

--- src/components/NavigationMobile.vue
+++ src/components/NavigationMobile.vue
@@ -36,8 +36,8 @@
 					class="flex items-center justify-center text-center mt-2 md:mt-0 py-3 w-full md:ml-2 font-satoshi font-bold text-sm md:text-base text-brandPrimaryWhite border border-brandNeutral-800 dark:border-brandPrimaryWhite bg-brandNeutral-800 rounded-2xl drop-shadow-xl shadow-brandPrimaryBlack">
 					<span class="relative flex h-3 w-3 mr-4">
 						<span
-							class="animate-ping absolute inline-flex h-full w-full rounded-full bg-brandSecondaryRed opacity-75"></span>
-						<span class="relative inline-flex rounded-full h-3 w-3 bg-brandSecondaryRed"></span>
+							class="animate-ping absolute inline-flex h-full w-full rounded-full bg-brandSecondaryGreen opacity-75"></span>
+						<span class="relative inline-flex rounded-full h-3 w-3 bg-brandSecondaryGreen"></span>
 					</span>
 					<span>Available to work</span>
 				</div>
