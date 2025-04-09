429d1857f31af4da47175cbb8b1658aa56904612
Yang Edruce <nurulizyansyaza@gmail.com>
2025-04-10 01:40:58 +0800

    Change /docs to /disc

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
 	plugins: [vue()],
 	base: "/",
-	build: {
-		outDir: "./docs",
-	},
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
